b536f4ba37172c66d83afbd09d57cf2d115dfafe
Zhishi <whfuyn@outlook.com>
2022-02-21 19:33:23 +0800

    refactor config design

--- src/config.rs
+++ src/config.rs
@@ -1,47 +1,88 @@
 use std::path::Path;
+use std::path::PathBuf;
 
 use serde::{Deserialize, Serialize};
 use anyhow::Result;
+use std::io::Write;
+use std::fs;
+use std::fs::File;
 
-mod default {
-    pub fn controller_addr() -> String {
-        "localhost:50005".into()
-    }
+use tempfile::NamedTempFile;
 
-    pub fn executor_addr() -> String {
-        "localhost:50002".into()
-    }
-
-    pub fn default_account() -> String {
-        "default".into()
-    }
-
-    pub fn data_dir() -> String {
-        let home = home::home_dir().expect("cannot find home dir");
-        home.join(".cloud-cli-v0.3.0").to_string_lossy().to_string()
-    }
-}
+const CLOUD_CLI_CONFIG_FILE_NAME: &str = "config.toml";
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
-    #[serde(default = "default::controller_addr")]
-    pub controller_addr: String,
-    #[serde(default = "default::executor_addr")]
-    pub executor_addr: String,
+    // The dir containing this config file
+    #[serde(skip)]
+    pub data_dir: PathBuf,
 
-    #[serde(default = "default::default_account")]
-    pub default_account: String,
-    #[serde(default = "default::data_dir")]
-    pub data_dir: String,
+    pub default_context: String,
+    pub contexts: Vec<ContextConfig>,
+}
+
+impl Config {
+    pub fn open(data_dir: impl AsRef<Path>) -> Result<Self> {
+        let data_dir = data_dir.as_ref();
+        fs::create_dir_all(data_dir)?;
+
+        let config_file = data_dir.join(CLOUD_CLI_CONFIG_FILE_NAME);
+        let mut config = if config_file.exists() {
+            let s = fs::read_to_string(config_file)?;
+            toml::from_str(&s)?
+        } else {
+            let mut f = File::create(config_file)?;
+            let default_config = Self::default();
+            f.write_all(toml::to_string_pretty(&default_config).unwrap().as_bytes())?;
+
+            default_config
+        };
+
+        config.data_dir = data_dir.to_path_buf();
+
+        Ok(config)
+    }
+
+    // atomically save
+    pub fn save(&self) -> Result<()> {
+        let tmp = NamedTempFile::new_in(self.data_dir)?;
+        let content = toml::to_string_pretty(self)?;
+        tmp.write_all(content.as_bytes())?;
+        let f = tmp.persist(Path::new(&self.data_dir).join(CLOUD_CLI_CONFIG_FILE_NAME))?;
+        f.flush()?;
+        Ok(())
+    }
 }
 
 impl Default for Config {
     fn default() -> Self {
+        let data_dir = {
+            let home = home::home_dir().expect("cannot find home dir");
+            home.join(".cloud-cli-v0.3.0")
+        };
         Self {
-            controller_addr: default::controller_addr(),
-            executor_addr: default::executor_addr(),
-            default_account: default::default_account(),
-            data_dir: default::data_dir(),
+            default_context: "default".into(),
+            data_dir,
+            contexts: vec![ContextConfig::default()],
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ContextConfig {
+    name: String,
+    controller_addr: String,
+    executor_addr: String,
+    account_id: String,
+}
+
+impl Default for ContextConfig {
+    fn default() -> Self {
+        Self {
+            name: "default".into(),
+            controller_addr: "localhost:50004".into(),
+            executor_addr: "localhost:50002".into(),
+            account_id: "default".into(),
         }
     }
 }
