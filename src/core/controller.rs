c1f7074ab49778b3e5ea65676f9bb94799d1a22d
hujiang <a15174027322@icloud.com>
2022-06-06 17:11:30 +0800

    refactor: add quota limit

--- src/core/controller.rs
+++ src/core/controller.rs
@@ -294,7 +294,7 @@ pub enum UtxoType {
     Validators = 1004,
     EmergencyBrake = 1005,
     BlockLimit = 1006,
-    PackageLimit = 1007,
+    QuotaLimit = 1007,
 }
 
 #[tonic::async_trait]
@@ -390,7 +390,7 @@ where
                 UtxoType::BlockInterval => &system_config.block_interval_pre_hash,
                 UtxoType::Validators => &system_config.validators_pre_hash,
                 UtxoType::EmergencyBrake => &system_config.emergency_brake_pre_hash,
-                UtxoType::PackageLimit => &system_config.package_limit_pre_hash,
+                UtxoType::QuotaLimit => &system_config.quota_limit_pre_hash,
                 UtxoType::BlockLimit => &system_config.block_limit_pre_hash,
             }
             .clone();
