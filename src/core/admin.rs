8270a79951b48fe83370a2b493cf3b5d70bd8e7a
Jayanring <956165026@qq.com>
2022-09-07 11:36:32 +0800

    remove admin set-block-kimit

--- src/core/admin.rs
+++ src/core/admin.rs
@@ -36,9 +36,6 @@ pub trait AdminBehaviour {
     async fn set_quota_limit<S>(&self, admin_signer: &S, quota_limit: u64) -> Result<Hash>
     where
         S: SignerBehaviour + Send + Sync;
-    async fn set_block_limit<S>(&self, admin_signer: &S, block_limit: u64) -> Result<Hash>
-    where
-        S: SignerBehaviour + Send + Sync;
 }
 
 #[tonic::async_trait]
@@ -103,15 +100,5 @@ where
         self.send_utxo(admin_signer, output, UtxoType::QuotaLimit)
             .await
             .context("failed to send `set_quota_limit` utxo")
-    }
-
-    async fn set_block_limit<S>(&self, admin_signer: &S, block_limit: u64) -> Result<Hash>
-    where
-        S: SignerBehaviour + Send + Sync,
-    {
-        let output = block_limit.to_be_bytes().to_vec();
-        self.send_utxo(admin_signer, output, UtxoType::BlockLimit)
-            .await
-            .context("failed to send `set_block_limit` utxo")
     }
 }
